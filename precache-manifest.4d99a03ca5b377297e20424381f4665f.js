bb9e1f1d9fae32e84d53238e5dba2dd3e37429bb
Ashin <ahmedashin999@gmail.com>
2020-08-09 15:35:00 +0600

    Updates

---
+++ precache-manifest.4d99a03ca5b377297e20424381f4665f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a187ca32b29fae84e4ec233eea71327b",
+    "url": "/Alan_AI_News/index.html"
+  },
+  {
+    "revision": "691a483fdfd511e88b44",
+    "url": "/Alan_AI_News/static/css/main.8ca58430.chunk.css"
+  },
+  {
+    "revision": "f0edbdc899aac361bdc9",
+    "url": "/Alan_AI_News/static/js/2.9306fa59.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Alan_AI_News/static/js/2.9306fa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691a483fdfd511e88b44",
+    "url": "/Alan_AI_News/static/js/main.58d3a401.chunk.js"
+  },
+  {
+    "revision": "5afa77f6d3b107a7a573",
+    "url": "/Alan_AI_News/static/js/runtime-main.acc31656.js"
+  }
+]);
